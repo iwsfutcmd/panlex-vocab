cc9f0491060e372ae852e901265ed735e1dafa97
Ben Yang <benayang@gmail.com>
2019-04-23 12:04:25 -0700

    more routing fixing

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ app = Sanic()
 app.static("/vocab/static", "./static")
 app.static("/favicon.ico", "./favicon.ico")
 
-@app.route("/vocab/<de_uid>/<al_uid>")
+@app.route("/<de_uid>/<al_uid>")
 async def main(request, de_uid, al_uid):
     template = Template(open("vocab.jinja2").read())
     try:
