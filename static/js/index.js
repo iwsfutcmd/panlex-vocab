0bd86d1729c3babf6d9f5cbd5acd69df7de573e0
Ben Yang <benayang@gmail.com>
2019-04-23 16:14:11 -0700

    fixed lang pickers

--- static/js/index.js
+++ static/js/index.js
@@ -1,10 +1,10 @@
 const deLangPicker = document.getElementById("de-lang-picker");
 deLangPicker.addEventListener("language-select", e => {
-  let alLang = location.pathname.replace(/\/$/, "").slice(-7);
-  location = `../../${e.currentTarget.dataset["uid"]}/${alLang}`;
+  let deLang = location.pathname.replace(/\/$/, "").slice(-15, -8);
+  location = location.href.replace(deLang, e.currentTarget.dataset["uid"]);
 });
 const alLangPicker = document.getElementById("al-lang-picker");
 alLangPicker.addEventListener("language-select", e => {
-  let deLang = location.pathname.replace(/\/$/, "").slice(-15, -8);
-  location = `../../${deLang}/${e.currentTarget.dataset["uid"]}`;
+  let alLang = location.pathname.replace(/\/$/, "").slice(-7);
+  location = location.href.replace(alLang, e.currentTarget.dataset["uid"]);
 });
